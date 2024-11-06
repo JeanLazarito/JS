7b07967c469d15eccf7116a13cba4e12155e84be
JeanLazarito <lazarito.jean.j@gmail.com>
2024-11-06 21:40:12 +0800

    Add files via upload

---
+++ average.js
@@ -0,0 +1,4 @@
+let x = parseInt(prompt("Enter your x: "));
+let y = parseInt(prompt("Enter your y: "));
+let average = (x + y)/2;
+console.log("Average: " + average.toFixed(2));
---
+++ gpa.js
@@ -0,0 +1,4 @@
+let GPA = parseFloat(prompt("Enter your GPA: "));
+let height = parseFloat(prompt("Enter your height: "));
+let weight = parseFloat(prompt("Enter your Weight: "));
+console.log("GPA: "+ GPA.toFixed(2) + "\nHeight: " + height.toFixed(2) + "\nWeight: " + weight.toFixed(2));
